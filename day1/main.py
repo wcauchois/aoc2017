1220bf94c40889377b9a806fe4277ddca9f5f453
Bill Cauchois <wcauchois@gmail.com>
2017-12-01 00:06:50 -0500

    star2

--- day1/main.py
+++ day1/main.py
@@ -2,9 +2,10 @@
 def thing(s):
   cur = None
   summ = 0
+  lenby2 = int(len(s) / 2)
   for i in range(0, len(s)):
     cur = s[i]
-    nex = s[(i + 1) % len(s)]
+    nex = s[(i + lenby2) % len(s)]
     if cur == nex:
       summ += int(cur)
   return summ
